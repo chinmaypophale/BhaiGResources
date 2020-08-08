3096c101b4790df9fd2092064e280a56b6beeac0
chinmaypophale <37659306+chinmaypophale@users.noreply.github.com>
2020-08-08 15:08:01 +0530

    Add files via upload

---
+++ OnlineLinkGenerator.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        // String fruitsUrl="http://dexterlabs.co.in/BhaiG/fruits/f";
+         for (int i=1;i<=100;i++){
+            System.out.println("http://dexterlabs.co.in/BhaiG/category/c"+i+".jpg");     
+         }
+        
+     }
+}
